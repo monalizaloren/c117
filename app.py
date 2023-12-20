ad2dc33682c5ab75875da213a268aadeb7b36843
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-12-20 20:21:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-from flask import Flask, render_template, url_for, request, jsonify
-from text_sentiment_prediction import *
 
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return render_template('///')
- 
-@app.route('/predict-emotion', methods=["POST"])
-
-
-
-    
